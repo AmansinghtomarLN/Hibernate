47452cb05b4e162b82f17a967addc441899aa1c0
Aman Singh Tomar <amansinghtomar2209@gmail.com>
2021-11-17 03:17:16 +0530

    git is imported

--- Demo/src/main/java/Registration/Demo/App.java
+++ Demo/src/main/java/Registration/Demo/App.java
@@ -12,7 +12,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        
+        System.out.println("git project");
         Persistence p = new Persistence();
         
         p.setName("Amannn");
